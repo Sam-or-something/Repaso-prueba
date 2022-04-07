7f5fb8d6404721f8459ebe7105121c8f50843e39
Sam <89226291+Sam-or-something@users.noreply.github.com>
2022-04-07 08:33:36 -0300

    Exercise 1 & 2 complete

---
+++ Assets/Scripts/Ej1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //Crea un programa que pida dos números al usuario y responda si son iguales o no lo son.
+    public int num1, num2;
+
+    void Start()
+    {
+        if (num1 == num2)
+        {
+            Debug.Log("Los numeros son iguales");
+        }
+        else
+        {
+            Debug.Log("Los numeros no son iguales");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Ej2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //Solicitar el ingreso del precio de 3 productos y un monto de dinero disponible.
+    //Comunicar si la suma de los mismos supera o no el monto de dinero disponible.
+    //Luego indicar cuánto dinero sobra o falta.
+    public float precio1, precio2, precio3, disponible;
+    float sobra, falta;
+    void Start()
+    {
+        sobra = disponible - (precio1 + precio2 + precio3);
+        if (sobra >= 0)
+        {
+            Debug.Log("El precio total no es mayor que el dinero disponible, sobran " + sobra + " pesos.");
+        }
+        else
+        {
+            falta = (precio3 + precio2 + precio1) - disponible;
+            Debug.Log("El precio total es mayor que el dinero disponible, faltan " + falta + " pesos.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
